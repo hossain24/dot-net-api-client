7e55661686e4e74afa0c98f3697a4ec86f49e651
hossain24 <haj.ctg@gmail.com>
2019-03-08 01:57:10 +0100

    CompanyDetail class added

---
+++ ApiConsole/CompanyDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiConsole
+{
+    public class CompanyDetail
+    {
+        public string id { get; set; }
+        public string companyId { get; set; }
+        public string name { get; set; }
+        public string companyEmail { get; set; }
+        public string phoneNumber { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string employeeEmail { get; set; }
+        public string title { get; set; }
+        public string customerName { get; set; }
+        public string customerValue { get; set; }
+    }
+}
